8d2bdbb5d385c1b4da48e0d40a1195f8143e00ec
Jondri <jondriellewilson@gmail.com>
2025-09-17 12:37:51 -0500

    Fixing logo image import for docker

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,10 +1,14 @@
+<script setup>
+import logo from "@/assets/MilkAndGigglesLogo.png"
+</script>
+
 <template>
 <footer class="bg-white dark:bg-gray-900">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
               <a href="https://flowbite.com/" class="flex items-center">
-                  <img src="/src/assets/MilkAndGigglesLogo.png" alt="Test Logo" class="h-20 w-25 me-3"/>
+                  <img :src="logo" alt="Test Logo" class="h-20 w-25 me-3"/>
                   <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Milk & Giggles</span>
               </a>
           </div>
